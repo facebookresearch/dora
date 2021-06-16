25e58a8405bf969bce9768fa725812e942931cd2
Alexandre Defossez <alexandre.defossez@gmail.com>
2021-06-16 16:00:16 +0200

    weird

--- dora/conf.py
+++ dora/conf.py
@@ -15,8 +15,10 @@ def update_from_args(data: tp.Any, args: Namespace):
     """Update the given dataclass from the argument parser args.
     """
     for key in data.__dict__:
-        if hasattr(args, key) and getattr(args, key) is not None:
-            setattr(data, key, getattr(args, key))
+        if hasattr(args, key):
+            value = getattr(args, key)
+            if value is not None:
+                setattr(data, key, value)
 
 
 def update_from_hydra(data: tp.Any, cfg: DictConfig):
