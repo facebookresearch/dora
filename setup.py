ae288fe845c1113b5b5c8477dc9121de28c6e57b
Alexandre Defossez <alexandre.defossez@gmail.com>
2021-09-07 15:27:50 +0200

    bump version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/fairinternal/dora'
 EMAIL = 'defossez@fb.com'
 AUTHOR = 'Alexandre Défossez'
 REQUIRES_PYTHON = '>=3.7.0'
-VERSION = "0.1.3dev1"
+VERSION = "0.1.4"
 
 HERE = Path(__file__).parent
 
