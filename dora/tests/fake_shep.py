1d0ffddc3155950ef15dda5789967b7149c36167
Alexandre Defossez <alexandre.defossez@gmail.com>
2021-11-02 05:59:34 -0700

    fix test

--- dora/tests/fake_shep.py
+++ dora/tests/fake_shep.py
@@ -35,6 +35,10 @@ class FakeJob:
     def _state(self):
         return self.watcher.get_state(self.job_id)
 
+    @property
+    def state(self):
+        return self.watcher.get_state(self.job_id)
+
     @_state.setter
     def _state(self, state: str):
         self.watcher.jobs[self.job_id] = state
