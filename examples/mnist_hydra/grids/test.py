d31aee19811a51d1a06576dcc8cc244d011b7b1d
Louis Martin <louismartincs@gmail.com>
2022-04-14 10:06:07 +0100

    Add an example of grid with hydra config like "model.hidden_dim=512" (#24)

--- examples/mnist_hydra/grids/test.py
+++ examples/mnist_hydra/grids/test.py
@@ -22,6 +22,9 @@ def explorer(launcher):
     for bs in [32, 64, 128]:
         launcher(batch_size=bs)
 
+    for hidden_dim in [512, 1024]:
+        launcher.bind_([f"model.hidden_dim={hidden_dim}"])
+
     launcher.bind_(gamma=0.6)
     launcher.slurm_(mem_per_gpu=20)
     launcher()
